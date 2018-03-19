ccc10c21ed8b297dda863fcb584d9da77809c90b
¨Gober¨ <reyesfragosoroberto@gmail.com>
2018-03-19 15:04:25 -0600

    redimensión

---
+++ scripts.js
@@ -0,0 +1,14 @@
+  /* var matrix = [[1,2,3],[4,5,6],[7,8,9]];
+                for (i=0;i<=2;i++){
+                    for(a=0;a<=2;a++){
+                        document.write(matrix[i][a] + " ====");
+                        document.write("\n " +i + " " + a  + "\n       ,");
+                    }
+                }*/
+
+                for (i = 1; i <= 3; i++) {
+                    for (a = 1; a <= 3; a++) {
+                        document.write(i * a);
+                    }
+        
+                }
